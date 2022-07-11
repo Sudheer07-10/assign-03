ce3e639e1714cf65ea17e73e836dc25781f7d5ce
SUDHEER <106311291+Sudheer07-10@users.noreply.github.com>
2022-07-11 22:26:51 +0530

    Update 04_manipulate_string.py

--- 04_manipulate_string.py
+++ 04_manipulate_string.py
@@ -3,9 +3,9 @@
 # Manipulate string
 
 '''
-Given a string S and a character C (as string of length 1), return a string with the characters surrounding any occurrence of C reversed. 
+Given a string S and a character C (as string of length 1), return a string with the characters surrounding any occurrence of C reversed.
 
-For example, if S is "Hello beautiful world" and C is a space, return "Hellb oeautifuw lorld". 
+For example, if S is "Hello beautiful world" and C is a space, return "Hellb oeautifuw lorld".
 
 Skip any positions where C occurs twice in a row.
 '''
@@ -14,6 +14,18 @@ def manipulate_string(S,C):
   i = 0
   ansst = ""
   # write your code here
+  for j in S:
+    if j == C:
+      i += 1
+  if i == 1:
+    a = S.find(C)
+    ansst = S[:a-1] + S[a+1] + C + S[a-1] + S[a+2:]
+    return ansst
+  elif i >1:
+    a = S.find(C)
+    x = S[:a-1] + S[a+1] + C + S[a-1] + S[a+2:]
+    v = S.rfind(C)
+    ansst = x[:v-1] + x[v+1] + C + x[v-1] + x[v+2:]
   return ansst
 
 
